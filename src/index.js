61b8ad939fe0ae75060870ec181fb7aac98dda5d
Regis Boudinot <selfup@users.noreply.github.com>
2017-12-16 13:09:21 -0700

    intial commit
    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { app } from 'hyperapp';
+import actions from './actions';
+import state from './state';
+import view from './components/Counter';
+
+const model = {
+  state,
+  actions,
+};
+
+const {
+  actions: dispatch,
+} = app(
+  model,
+  view,
+  document.body,
+);
+
+/**
+ * hyperapp exposes its actions
+ * i like to see it as a dispatch
+ * this is just an example of calling the actions from outside of hyperapp!!
+ * think of it as a hub to talk to other apps/frameworks/vanillaJS
+ * here is an example on codepen: https://codepen.io/selfup/pen/jLMRjO
+ */
+setTimeout(dispatch.add, 1000);
+setTimeout(dispatch.sub, 2000);
