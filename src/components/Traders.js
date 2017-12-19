30842cbe35baacc8578c3cb481a6723cebe4d988
selfup <boudinot.regis@yahoo.com>
2017-12-19 02:36:56 -0700

    Remove if statement for Traders

--- src/components/Traders.js
+++ src/components/Traders.js
@@ -1,16 +1,6 @@
 import { h } from 'hyperapp';
 
 export default (str) => {
-  if (str === 'No One') {
-    return (
-      <ul>
-        <li>
-          <em>{str}</em>
-        </li>
-      </ul>
-    );
-  }
-
   const items = str.split(',');
 
   const list = items.map(item => <li>{item}</li>);
